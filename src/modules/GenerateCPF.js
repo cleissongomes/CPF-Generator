55f62b875c9da68e5b79f9258640768a4397deb8
cleissongomes <cleissongomes777@gmail.com>
2023-05-31 13:29:07 -0300

    feat: Add GenerateCPF class with rand and generateNewCpf methods

--- src/modules/GenerateCPF.js
+++ src/modules/GenerateCPF.js
@@ -1,1 +1,15 @@
 import ValidCPF from './ValidCPF';
+
+export default class GenerateCPF {
+    rand(min = 100000000, max = 99999999) {
+        return String(Math.floor(Math.ramdom() * (max -min) + min));
+    }
+
+    generateNewCpf() {
+        const cpfWithoutDigits = this.rand();
+        const digit1 = ValidCPF.generateDigit(cpfWithoutDigits);
+        const digit2 = ValidCPF.generateDigit(cpfWithoutDigits + digit1);
+        const newCpf = cpfWithoutDigits + digit1 + digit2;
+        return newCpf;
+    }
+}
